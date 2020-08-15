9e52b1a7085f44423fef71919db23419f3932cd5
rodrigo.actavares@gmail.com <rodrigo.actavares@gmail.com>
2020-08-15 12:30:50 +0100

    Adding sh scripts

--- manifests/prometheus.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# A sample script to install prometheus using helm
-helm install prometheus stable/prometheus --namespace prometheus --set alertmanager.persistentVolume.storageClass="gp7" --set server.persistentVolume.storageClass="gp2"
